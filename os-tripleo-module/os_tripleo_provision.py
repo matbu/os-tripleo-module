972500fdcf045a86120b5f5ffa7fea7966c51560
Mathieu Bultel <mbultel@redhat.com>
2016-01-08 00:08:10 +0100

    add ansible module for provision tripleo

---
+++ os-tripleo-module/os_tripleo_provision.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+#coding: utf-8 -*-
+
+from provision import Provision
+
+DOCUMENTATION = '''
+---
+module: os-tripleo-privision
+   - POC
+options:
+'''
+
+EXAMPLES = '''
+
+'''
+
+_os_keystone   = None
+_os_tenant_id  = None
+_os_network_id = None
+_inc = 0
+
+def _get_provision(module, kwargs):
+    try:
+        provision = Provision(repolist=kwargs.get('repo'))
+    except Exception, e:
+        module.fail_json(msg = "Error : %s" %e.message)
+    global _os_provision
+    _os_provision = provision
+    return provision
+
+def _provision(module):
+    try:
+        return _os_provision.provision()
+    except Exception, e:
+        module.fail_json(msg = "Error : %s" %e.message)
+
+def _is_instack(module):
+    return _os_provision.is_instack()
+
+def main():
+
+    argument_spec = openstack_argument_spec()
+    argument_spec.update(dict(
+            repo                    = dict(Default=None),
+            state                   = dict(default='present', choices=['absent', 'present']),
+    ))
+    module = AnsibleModule(argument_spec=argument_spec)
+
+    if module.params['state'] == 'present':
+        if _is_instack(module):
+            module.exit_json(changed = False, result = "Success" )
+        else:
+            instack_ip = _is_instack(module)
+            module.exit_json(changed = True, result = "Created" , ip = instack_ip)
+    else:
+        if _is_instack(module):
+            module.exit_json(changed = True, result = "delete")
+        else:
+            module.exit_json(changed = False, result = "Success")
+
+# this is magic, see lib/ansible/module.params['common.py
+from ansible.module_utils.basic import *
+from ansible.module_utils.openstack import *
+main()
